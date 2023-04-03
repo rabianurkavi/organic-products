f690f09107238532bd5ca9fb8ef35b93a9d26bd8
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-04-03 21:19:27 +0300

    search component edit

--- components/ui/Search.jsx
+++ components/ui/Search.jsx
@@ -10,7 +10,7 @@ const Search = ({setIsSearchModal}) => {
     after:w-screen after:h-screen after:bg-white after:absolute after:top-0 after:left-0 after:opacity-60 grid place-content-center">
      <OutsideClickHandler onOutsideClick={()=>setIsSearchModal(false)}>
        <div className="w-full h-full grid place-content-center relative ">
-         <div className="relative z-50 w-[500px] h-[560px] bg-white border-2 p-5">
+         <div className="relative z-50 md:w-[500px] w-[370px] bg-white border-2 p-10 rounded 3-xl">
           <Title addClass="text-[40px] text-center">Arama</Title>
           <input type="text" placeholder="Search..." className="border w-full my-10"/>
             <ul className=''>
