2839e034e170e94cc524d78b266a346f7a59ec30
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-04-08 20:22:23 +0300

    Campaigns edit

--- components/ui/Campaigns.jsx
+++ components/ui/Campaigns.jsx
@@ -2,25 +2,29 @@ import Image from 'next/image'
 import React from 'react'
 const CampaignsItem = ()=>{
     return (
-        <div className="">
-            <div className="relative w-96 h-60 ">
+        <div className="container mx-auto">
+            <div className="px-40">
+            <div className=" relative w-96 h-60 ">
                 <Image className='hover:scale-105 transition-all' src="/images/img-01.jpg" alt='' layout='fill' />
+            </div>
             </div>
         </div>
     )
 }
 const CampaignsItem2 = ()=>{
     return (
-        <div className="">
-            <div className="relative w-96 h-60 ">
+        <div className="container mx-auto ">
+            <div className="px-40">
+             <div className=" relative w-96 h-60 ">
                 <Image className='hover:scale-105 transition-all' src="/images/img-02.jpg" alt='' layout='fill' objectFit='cover' />
+             </div>
             </div>
         </div>
     )
 }
 const Campaigns = () => {
   return (
-    <div className='flex container mx-auto py-20 flex-wrap gap-x-80'>
+    <div className='mt-8 flex bg-slate-100 py-20 justify-center'>
         <CampaignsItem />
         <CampaignsItem2 />
     </div>
