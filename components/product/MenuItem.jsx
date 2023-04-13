4265e022a3efdb46ca83f905d029a768a1739112
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-04-13 23:51:22 +0300

    edit

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -4,18 +4,18 @@ import {MdShoppingBasket} from "react-icons/md"
 
 const MenuItem = () => {
     return (
-      <div className="bg-secondary ">
-          <div className='w-full bg-primary h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl '>
-            <div className="relative w-[280px] h-[210px] hover:scale-110 transition-all">
-              <Image src="/images/olive.jpg" alt="" layout="fill" objectFit='cover' className="rounded-xl rounded-bl-[40px]"   />
+      <div className="bg-secondary w-[270px] ">
+          <div className='w-full bg-primary h-[200px]  grid place-content-center rounded-bl-[46px]  '>
+            <div className="relative w-[270px] h-[210px] hover:scale-110 transition-all">
+              <Image src="/images/paste.jpg" alt="" layout="fill" objectFit='cover' className=" rounded-bl-[40px]"   />
             </div>
           </div>
           <div className="p-[25px] text-black">
-            <h4 className="text-xl font-sans">Hal Hal Zeytin</h4>
-            <p className="text-[13px]">Doğal ürünlerimizden hal hal zeytin Hatay dan gelmektedir.</p>
+            <h4 className="text-xl font-sans">Salça</h4>
+            <p className="text-[13px]">Doğal ürünlerimizden el yapımı salça anne eli değmiş gibi :P.</p>
           
             <div className="flex justify-between items-center mt-4">
-             <button className='btn-black rounded-full flex items-center gap-2 font-sans font-semibold hover:bg-primary'>
+             <button className='btn-black flex items-center gap-2 font-sans font-semibold hover:bg-primary'>
               Sepete Ekle<MdShoppingBasket /></button>
              <span className='text-primary font-bold'>200₺</span>
              </div>
--- components/ui/Campaigns.jsx
+++ components/ui/Campaigns.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CampaignsItem = ()=>{
     return (
         <div className="container mx-auto">
-            <div className="px-40">
+            <div className="px-60">
             <div className=" relative w-96 h-60 ">
                 <Image className='hover:scale-105 transition-all' src="/images/img-01.jpg" alt='' layout='fill' />
             </div>
@@ -14,7 +14,7 @@ const CampaignsItem = ()=>{
 const CampaignsItem2 = ()=>{
     return (
         <div className="container mx-auto ">
-            <div className="px-40">
+            <div className="px-[50px]">
              <div className=" relative w-96 h-60 ">
                 <Image className='hover:scale-105 transition-all' src="/images/img-02.jpg" alt='' layout='fill' objectFit='cover' />
              </div>
